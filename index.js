ec9cb6960788fc4af704fcd2fa7a6e4c95f07a2f
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-29 18:21:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter( e =>
+        e.toLowerCase() === name.toLowerCase()
+    );
+}
+
+function fuzzyMatch(drivers, startingLetters) {
+    return drivers.filter( e =>
+        e.toLowerCase().indexOf(startingLetters.toLowerCase()) === 0
+    )
+}
+
+function matchName(drivers, searchedName) {
+    return drivers.filter(e =>
+        e.name === searchedName
+    )
+} 
